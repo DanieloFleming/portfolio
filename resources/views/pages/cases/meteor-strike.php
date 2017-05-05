ddd3645736ece3d69715526b5f864dfd651f6664
Danielo Fleming <danielofleming@gmail.com>
2017-05-05 09:53:39 +0000

    removed redundant files

--- resources/views/pages/cases/meteor-strike.php
+++ resources/views/pages/cases/meteor-strike.php
@@ -23,28 +23,12 @@
         </div>
     </div>
     <div class="section" style="background:url('/static/img/cases/meteor-strike/blue.png'); repeat;">
-        <img data-delay="0" src="/static/img/cases/meteor-strike/sprite3.png" style="width:100%;" class="smart-object layer_middle"/>
-<!--
-        <div class="layer_group layer_front">
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_05.png" style="position:absolute; left:0; top:0;">
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_06.png" style="position:absolute; left:210%; top:130px;">
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_08.png" style="position:absolute; left:89%; top:150px;">
-        </div>
-        <div class="layer_group layer_middle">
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_05.png" style="position:absolute; left:40%; top:0;">
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_06.png" style="position:absolute; left:11%; top:130px;">
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_07.png" style="position:absolute; left:60%; top:210px;">
-        </div>
-        <div class="layer_group layer_back">
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_07.png" style="position:absolute; left:34%; top:210px;">
-
-            <img src="/static/img/cases/meteor-strike/sliced/sprite_08.png" style="position:absolute; left:70%; top:150px;">
-        </div>-->
+        <img data-delay="0" src="/static/img/cases/meteor-strike/sprite3.png" style="width:100%;" class="smart-object"/>
     </div>
     <div class="section is-aligned--center is-unpadded" style="background:#3A2E3F">
         <div class="row is-unpadded">
             <div class="column is-unpadded">
-                <iframe src="<?= url('/static/demos/mstrike/test.html');?>" style="width:100%; height:100%; max-width:800px;position:absolute;top:0"></iframe>
+                <iframe src="<?= url('/static/demos/mstrike/test.html');?>" style="width:100%; height:100%; max-width:800px;position:absolute;top:0; overflow: hidden"></iframe>
                 <canvas width="800" height="600" style="width:100%; max-width:800px;  visibility: hidden"></canvas>
             </div>
         </div>
